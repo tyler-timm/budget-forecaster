13b24488444a6fc781e0137c4cb48fb75dfcd8ec
Tyler Timm <ttimm16@gmail.com>
2024-09-23 10:49:19 -0500

    Add responsive nav

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -6,14 +6,22 @@
 	<div class="inner-container">
 		<img src="./flying-piggy-bank.png" alt="flying piggy bank" width="60" height="60" />
 		<h1>Budget Forecaster</h1>
+		<nav class="fullscreen-nav">
+			<a href="/">Home</a>
+			<a href="/recurring">Recurring Transactions</a>
+		</nav>
 		<Signout />
 	</div>
+	<nav class="mobile-nav">
+		<a href="/">Home</a>
+		<a href="/recurring">Recurring Transactions</a>
+	</nav>
 </div>
 
 <style>
 	.container {
 		display: flex;
-        position: sticky;
+		position: sticky;
 		flex-direction: column;
 		/* align-items: center; */
 		top: 0px;
@@ -26,6 +34,24 @@
 		display: flex;
 		align-items: center;
 		margin-left: 2rem;
+	}
+
+	.fullscreen-nav {
+		display: none;
+	}
+
+    .mobile-nav {
+        padding: 1rem;
+    }
+
+	a {
+		/* font-family: 'Noto Sans', sans-serif; */
+		font-size: 1.2rem;
+		line-height: 1;
+		font-weight: 700;
+		padding: 1.25rem;
+		color: #eab308;
+		text-decoration: none;
 	}
 
 	h1 {
@@ -45,6 +71,20 @@
 			padding: 1.25rem;
 			color: #eab308;
 		}
+	}
+
+	@media (min-width: 915px) {
+		.fullscreen-nav {
+			display: block;
+		}
+
+		.mobile-nav {
+			display: none;
+		}
+
+        a {
+            font-size: 1.5rem;
+        }
 	}
 
 	img {
