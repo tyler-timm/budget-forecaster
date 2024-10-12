f593253095c17ef29262842cd5c0dc6a033fe6c1
Tyler Timm <ttimm16@gmail.com>
2024-10-11 23:26:12 -0500

    Fix footer on mobile

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -26,15 +26,20 @@
 		background-color: #264653;
 	}
 
+	ul {
+		display: flex;
+		gap: 1.5rem;
+	}
+
 	li {
 		list-style-type: none;
 		color: #eab308;
 		font-size: 2rem;
 	}
 
-    iconify-icon {
-        padding-top: 0.5rem;
-    }
+	iconify-icon {
+		padding-top: 0.5rem;
+	}
 
 	.author {
 		font-size: 1.5rem;
@@ -45,9 +50,6 @@
 
 	@media (min-width: 915px) {
 		ul {
-			display: flex;
-			flex-direction: row;
-			gap: 1.5rem;
 			margin-top: 1rem;
 			margin-bottom: 1rem;
 		}
