c00add8dd0fc78bcacc55155421483ccdc0978dd
Tyler Timm <ttimm16@gmail.com>
2024-10-15 14:01:14 -0500

    Add Netlify, Neon, Postgres icons to tech used

--- src/routes/tech/+page.svelte
+++ src/routes/tech/+page.svelte
@@ -8,17 +8,32 @@
 		<ul>
 			<li>
 				<a href="https://svelte.dev/">
-					<iconify-icon icon="ri:svelte-fill"></iconify-icon> Svelte + SvelteKit</a
+					<iconify-icon icon="ri:svelte-fill" /> Svelte + SvelteKit</a
 				>
 			</li>
 			<li>
 				<a href="https://lucia-auth.com/"
-					><iconify-icon icon="simple-icons:lucia"></iconify-icon> Lucia</a
+					><iconify-icon icon="simple-icons:lucia" /> Lucia</a
 				>
 			</li>
 			<li>
 				<a href="https://iconify.design/"
-					><iconify-icon icon="simple-icons:iconify"></iconify-icon> Iconify</a
+					><iconify-icon icon="simple-icons:iconify" /> Iconify</a
+				>
+			</li>
+			<li>
+				<a href="https://www.postgresql.org/"
+					><iconify-icon icon="simple-icons:postgresql" /> Postgres</a
+				>
+			</li>
+			<li>
+				<a href="https://neon.tech/"
+					><iconify-icon icon="logos:neon-icon" /> Neon DB</a
+				>
+			</li>
+			<li>
+				<a href="https://www.netlify.com/"
+					><iconify-icon icon="simple-icons:netlify" /> Netlify</a
 				>
 			</li>
 		</ul>
